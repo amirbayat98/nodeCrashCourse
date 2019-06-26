63f9fe248def4fdc73777b063e07e8845254d845
amirbayat98 <amirbayat1998@yahoo.com>
2019-06-25 23:08:11 -0900

    amir1

---
+++ main.py
@@ -0,0 +1,135 @@
+import serial
+import time
+
+flag_serial = False
+
+#from flask import Flask, jsonify, render_template, request, send_from_directory
+from random import *
+import threading
+import time
+
+# app = Flask(__name__, static_folder="templates/digital/build/static", template_folder='templates/digital/build')
+#app = Flask(__name__, template_folder="templates1")
+
+
+nodFlag = False
+direction = ""
+
+ser1 = serial.Serial(port='/dev/ttyS0')
+ser1.baudrate = 115200
+
+
+try:
+    ser = serial.Serial('/dev/ttyUSB0')
+except serial.serialutil.SerialException:
+    flag_serial = False
+
+if flag_serial:
+    ser.baudrate = 38400
+    print(ser.name)
+data = [0] * 9
+max_speed = 150
+output_speedR = 0
+output_speedL = 0
+speed_step = 3
+speed_stepR = 15
+
+
+#@app.route('/')
+#def direction():
+#    # return direction
+#    global direction
+#    direction = request.args.get('direction')
+#    print(direction)
+#    #dir()
+#    if flag_serial:
+#        for i in range(9):
+#            data[i] = float(ser.readline().decode().strip())
+#        print(data[8])
+#    return render_template('ugv.html')
+
+
+# @app.route('/', methods=['POST'])
+# def direction1():
+#     print('goh')
+#     direction = request
+#     print(direction)
+#     return ("an")
+
+def dir():
+    global output_speedR, output_speedL
+    if direction == "w":
+        if (output_speedR < max_speed):
+            if output_speedR < 0:
+                output_speedR += speed_stepR
+            else:
+                output_speedR += speed_step
+        if (output_speedL < max_speed):
+            if output_speedL < 0:
+                output_speedL += speed_stepR
+            else:
+                output_speedL += speed_step
+        if flag_serial:
+            ser.write(("S" + str(output_speedR) + " " + str(output_speedL) + '\n').encode())
+
+    elif direction == "h":
+        output_speedL, output_speedR = 0, 0
+        if flag_serial:
+            ser.write(("S" + str(output_speedR) + " " + str(output_speedL) + '\n').encode())
+
+    elif direction == "s":
+        if (-output_speedR < max_speed):
+            if output_speedR > 0:
+                output_speedR -= speed_stepR
+            else:
+                output_speedR -= speed_step
+        if (-output_speedL < max_speed):
+            if output_speedL > 0:
+                output_speedL -= speed_stepR
+            else:
+                output_speedL -= speed_step
+        if flag_serial:
+            ser.write(("S" + str(output_speedR) + " " + str(output_speedL) + '\n').encode())
+
+    elif direction == "a":
+        if (output_speedR < max_speed):
+            output_speedR += speed_step
+        if (-output_speedL < max_speed):
+            output_speedL -= speed_step
+        if flag_serial:
+            ser.write(("S" + str(output_speedR) + " " + str(output_speedL) + '\n').encode())
+
+    elif direction == "d":
+        if (-output_speedR < max_speed):
+            output_speedR -= speed_step
+        if (output_speedL < max_speed):
+            output_speedL += speed_step
+        if flag_serial:
+            ser.write(("S" + str(output_speedR) + " " + str(output_speedL) + '\n').encode())
+
+
+    else:
+        if flag_serial:
+            ser.write(("S0 0\n").encode())
+
+    print(output_speedR, output_speedL)
+
+    time.sleep(0.1)
+
+
+
+while(True):
+	global direction
+	direc = ser1.readline()
+	print(direc)
+	direction = direc.decode().strip()
+	dir() 
+	
+
+
+
+#if __name__ == '__main__':
+#    if flag_serial:
+#        ser.write("R0 0\n".encode())
+#    app.debug = True
+#    app.run(use_reloader=True, host='0.0.0.0', port=5000)
